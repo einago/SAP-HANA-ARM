bbb8ed30618d25aef50888f8879d2f6d91a4d816
einago <31706350+einago@users.noreply.github.com>
2018-02-22 14:49:42 +0000

    Update hanastudio.ps1

--- hanastudio.ps1
+++ hanastudio.ps1
@@ -5,9 +5,9 @@ param (
     
     #Get the bits for the HANA installation and copy them to C:\SAPbits\SAP_HANA_STUDIO\
     $hanadest = "C:\SapBits"
-    $sapcarUri = $baseUri + "/SapBits/SAP_HANA_STUDIO/sapcar.exe" 
-    $hanastudioUri = $baseUri + "/SapBits/SAP_HANA_STUDIO/IMC_STUDIO2_212_2-80000323.SAR" 
-    $jreUri = $baseUri + "/SapBits/SAP_HANA_STUDIO/serverjre-9.0.1_windows-x64_bin.tar.gz" 
+    $sapcarUri = $baseUri + "/SaPBits/SAP_HANA_STUDIO/sapcar.exe" 
+    $hanastudioUri = $baseUri + "/SaPBits/SAP_HANA_STUDIO/IMC_STUDIO2_212_4-80000323.SAR" 
+    $jreUri = $baseUri + "/SaPBits/SAP_HANA_STUDIO/serverjre-9.0.4_windows-x64_bin.tar.gz" 
     $7zUri = "http://www.7-zip.org/a/7z1701-x64.msi"
     $sapcardest = "C:\SapBits\SAP_HANA_STUDIO\sapcar.exe"
     $hanastudiodest = "C:\SapBits\SAP_HANA_STUDIO\IMC_STUDIO2_212_2-80000323.SAR"
@@ -29,13 +29,13 @@ param (
     cd $jrepath
     .\7z.msi /quiet
     cd "C:\Program Files\7-Zip\"
-    .\7z.exe e "C:\Program Files\serverjre-9.0.1_windows-x64_bin.tar.gz" "-oC:\Program Files"
-    .\7z.exe x -y "C:\Program Files\serverjre-9.0.1_windows-x64_bin.tar" "-oC:\Program Files"
+    .\7z.exe e "C:\Program Files\serverjre-9.0.4_windows-x64_bin.tar.gz" "-oC:\Program Files"
+    .\7z.exe x -y "C:\Program Files\serverjre-9.0.4_windows-x64_bin.tar" "-oC:\Program Files"
     
     cd $hanapath
-    .\sapcar.exe -xfv IMC_STUDIO2_212_2-80000323.SAR
+    .\sapcar.exe -xfv IMC_STUDIO2_212_4-80000323.SAR
     
-    set PATH=%PATH%C:\Program Files\jdk-9.0.1\bin;
+    set PATH=%PATH%C:\Program Files\jdk-9.0.4\bin;
     set HDB_INSTALLER_TRACE_FILE=C:\Users\testuser\Documents\hdbinst.log
     cd C:\SAPbits\SAP_HANA_STUDIO\SAP_HANA_STUDIO\
     .\hdbinst.exe -a C:\SAPbits\SAP_HANA_STUDIO\SAP_HANA_STUDIO\studio -b --path="C:\Program Files\sap\hdbstudio"
