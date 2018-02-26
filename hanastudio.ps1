328e6bf7e7ba24061e6857369ae988fd87e0f4e5
einago <31706350+einago@users.noreply.github.com>
2018-02-26 14:20:01 +0000

    Update hanastudio.ps1

--- hanastudio.ps1
+++ hanastudio.ps1
@@ -9,13 +9,15 @@ param (
     $hanastudioUri = $baseUri + "/SaPBits/SAP_HANA_STUDIO/IMC_STUDIO2_212_4-80000323.SAR" 
     $jreUri = $baseUri + "/SaPBits/SAP_HANA_STUDIO/serverjre-9.0.4_windows-x64_bin.tar.gz" 
     $7zUri = "http://www.7-zip.org/a/7z1701-x64.msi"
+    $swpmUri = $baseUri + "/SaPBits/SapBits/SWPM10SP22_2-20009707.SAR"
     $sapcardest = "C:\SapBits\SAP_HANA_STUDIO\sapcar.exe"
     $hanastudiodest = "C:\SapBits\SAP_HANA_STUDIO\IMC_STUDIO2_212_4-80000323.SAR"
     $jredest = "C:\Program Files\serverjre-9.0.4_windows-x64_bin.tar.gz"
+    $swpmdest = "C:\SapBits\SWPM\SWPM10SP22_2-20009707.SAR"
     $7zdest = "C:\Program Files\7z.msi"
     $jrepath = "C:\Program Files"
     $hanapath = "C:\SapBits\SAP_HANA_STUDIO"
-    $hanajack = "C:\SapBits\Jack"
+    $hanajack = "C:\SapBits\SWPM"
     if((test-path $hanadest) -eq $false)
     {
         New-Item -Path $hanadest -ItemType directory
@@ -25,6 +27,7 @@ param (
     write-host "downloading files"
     Invoke-WebRequest $sapcarUri -OutFile $sapcardest
     Invoke-WebRequest $hanastudioUri -OutFile $hanastudiodest
+    Invoke-WebRequest $swpmUri -OutFile $swpmdest
     Invoke-WebRequest $jreUri -OutFile $jredest
     Invoke-WebRequest $7zUri -OutFile $7zdest
     
@@ -39,6 +42,8 @@ param (
     cd $hanapath
     cd >> c:\sapbits\jack.txt
     .\sapcar.exe -xfv IMC_STUDIO2_212_4-80000323.SAR >> c:\sapbits\jack.txt
+    cd $hanajack
+    ..\sapcar.exe -xfv SWPM10SP22_2-20009707.SAR
     
     set PATH=%PATH%C:\Program Files\jdk-9.0.4\bin;
     set HDB_INSTALLER_TRACE_FILE=C:\Users\testuser\Documents\hdbinst.log
